8fae3888a9bf720b15c6218525ceedebd316516f
Robério <roberiomatos@yahoo.com.br>
2022-09-10 10:03:23 -0300

    added get started, redux store, assets

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-// In App.js in a new project
-
-import * as React from "react";
 import RootRoutes from "./src/routes";
+import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
+
+import store from "./src/store";
 
 function App() {
-  return <RootRoutes />;
+
+  return(
+    <Provider store={store}>
+
+      <StatusBar backgroundColor="#272629" style="light" />
+      <RootRoutes />
+      
+    </Provider>
+  )
 }
 
 export default App;
