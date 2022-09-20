8d26ff7d4b380d19f8e37efdd121edfe2001ed73
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-09-20 06:44:36 -0300

    atualizações da screen 'perfil do dev'

--- src/routes/login.stack.tsx
+++ src/routes/login.stack.tsx
@@ -3,20 +3,27 @@ import GetStarted from "../views/GetStarted";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 import RecoverPassword from "../views/RecoverPassword";
+import ViewProfile from "../views/ViewProfile";
 
 function LoginStack() {
   return (
     <Navigator>
-        <Screen 
-          name="GetStarted" 
-          component={GetStarted} 
-          options={{ headerShown: false }}
-        />
+      {/* <Screen
+        name="GetStarted"
+        component={GetStarted}
+        options={{ headerShown: false }}
+      />
 
-        <Screen 
-          name="Recuperação de Senha" 
-          component={RecoverPassword} 
-        />
+      <Screen
+        name="Recuperação de Senha"
+        component={RecoverPassword}
+      /> */}
+
+      <Screen
+        name="Perfil do Dev"
+        options={{ headerShown: false }}
+        component={ViewProfile}
+      />
     </Navigator>
   );
 }
