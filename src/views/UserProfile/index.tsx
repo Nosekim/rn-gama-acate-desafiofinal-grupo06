6830ec8416a314236bc851b9932e253657ce2d5a
Nosekim <rogerfelipensk@gmail.com>
2022-09-22 19:18:21 -0300

    fix: Adicionado filtro na lista de devs

--- src/views/UserProfile/index.tsx
+++ src/views/UserProfile/index.tsx
@@ -1,9 +1,9 @@
 import { ScrollView, View } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { MaterialIcons, Ionicons, Entypo } from '@expo/vector-icons';
-import { Auth } from 'aws-amplify';
+import { MaterialIcons, Ionicons, Entypo } from "@expo/vector-icons";
+import { Auth } from "aws-amplify";
 
 import styles, { DataOption, TextData } from "./style";
 import { Label } from "../../global/GlobalStyles";
@@ -12,252 +12,172 @@ import { IAppState } from "../../types";
 
 import ProfilePicture from "../../components/ProfilePicture";
 
-import { 
-    changeIsLoggedIn,
-    changeEmail,
-    changePassword 
+import {
+  changeIsLoggedIn,
+  changeEmail,
+  changePassword,
 } from "../../store/modules/auth/reducer";
 
 interface IUserData {
-    title: string;
-    text: string;
-    screenTarget: string;
+  title: string;
+  text: string;
+  screenTarget: string;
 }
 
 export default function UserProfile() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const nav = useNavigation();
 
-    const nav = useNavigation();
+  const { email } = useSelector((state: IAppState) => state.auth);
 
-    const { email } = useSelector((state: IAppState) => state.auth);
+  const { name, photoUser, category, userStacks, description } = useSelector(
+    (state: IAppState) => state.user
+  );
 
-    const { name, photoUser, category, userStacks, description } = useSelector((state: IAppState) => state.user);
+  const textStacks = () => {
+    let text = "";
 
-    const textStacks = () => {
+    userStacks.forEach((item, i) => {
+      text += item;
 
-        let text = "";
+      if (i < userStacks.length - 1) text += "\n";
+    });
 
-        userStacks.forEach((item, i) => {
+    return text;
+  };
 
-            text += item;
+  const userData = ({ title, text, screenTarget }: IUserData) => {
+    if (text === "") text = "Não informado";
 
-            if(i < userStacks.length - 1)
-                text += "\n"; 
-        })
+    return (
+      <DataOption
+        onPress={() => nav.navigate(screenTarget)}
+        activeOpacity={0.3}
+      >
+        <View>
+          <Label>{title}</Label>
 
-        return text;
+          <TextData>{text}</TextData>
+        </View>
+
+        <AntDesign name="right" size={20} color="rgba(255, 255, 255, .3)" />
+      </DataOption>
+    );
+  };
+
+  const signOut = async () => {
+    const result = await Auth.signOut();
+
+    if (result) {
+      dispatch(changeIsLoggedIn(false));
+      dispatch(changeEmail(""));
+      dispatch(changePassword(""));
     }
+  };
 
-    const userData = ({ title, text, screenTarget }: IUserData) => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <ProfilePicture image={photoUser} updatePic={true} />
 
-        if(text === "")
-            text = "Não informado";
+      <View style={{ marginVertical: 20, width: "100%" }}>
+        {userData({
+          title: "Nome",
+          text: name,
+          screenTarget: "Editar Nome",
+        })}
 
-        return(
-            <DataOption
-                onPress={() => nav.navigate(screenTarget)}
-                activeOpacity={.3}
-            >
+        {userData({
+          title: "E-mail",
+          text: email,
+          screenTarget: "Editar E-mail",
+        })}
 
-                <View>
+        {userData({
+          title: "Senha",
+          text: "*******",
+          screenTarget: "Editar Senha",
+        })}
 
-                    <Label>{ title }</Label> 
+        {userData({
+          title: "Categoria",
+          text: category,
+          screenTarget: "Selecionar Categoria",
+        })}
 
-                    <TextData>{ text }</TextData>
+        {userData({
+          title: "Tecnologias",
+          text: textStacks(),
+          screenTarget: "Selecionar Tecnologias",
+        })}
 
-                </View>
-                
-                <AntDesign 
-                    name="right" 
-                    size={20} 
-                    color="rgba(255, 255, 255, .3)" 
-                />
+        {userData({
+          title: "Descrição",
+          text: description,
+          screenTarget: "Editar Descrição",
+        })}
 
-            </DataOption>
-        )
-    }
+        <View style={{ marginTop: 30 }}>
+          <Label>Mais opções</Label>
 
-    const signOut = async () => {
+          <DataOption
+            onPress={() => nav.navigate("Fale Conosco")}
+            activeOpacity={0.3}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <Entypo name="chat" size={20} color="rgba(255,255,255,.7)" />
 
-        const result = await Auth.signOut();
-
-        if(result) {
-
-            dispatch(changeIsLoggedIn(false));
-            dispatch(changeEmail(""));
-            dispatch(changePassword(""));
-        }
-    }
-
-    return(
-        <ScrollView
-            contentContainerStyle={styles.container}
-        >
-
-            <ProfilePicture 
-                image={photoUser}
-                updatePic={true}
-            />
-
-            <View style={{ marginVertical: 20, width: '100%' }}>
-
-                { 
-                    userData({
-                        title: "Nome",
-                        text: name,
-                        screenTarget: "Editar Nome"
-                    }) 
-                }
-
-                { 
-                    userData({
-                        title: "E-mail",
-                        text: email,
-                        screenTarget: "Editar E-mail"
-                    }) 
-                }    
-
-                { 
-                    userData({
-                        title: "Senha",
-                        text: "*******",
-                        screenTarget: "Editar Senha"
-                    }) 
-                }     
-
-                { 
-                    userData({
-                        title: "Categoria",
-                        text: category,
-                        screenTarget: "Selecionar Categoria" 
-                    }) 
-                }     
-
-                { 
-                    userData({
-                        title: "Tecnologias",
-                        text: textStacks(),
-                        screenTarget: "Selecionar Tecnologias" 
-                    }) 
-                }
-
-                { 
-                    userData({
-                        title: "Descrição",
-                        text: description,
-                        screenTarget: "Editar Descrição" 
-                    }) 
-                }
-
-                <View style={{ marginTop: 30 }}>
-
-                    <Label>Mais opções</Label> 
-
-                    <DataOption
-                        onPress={() => nav.navigate("Fale Conosco")}
-                        activeOpacity={.3}
-                    >
-
-                        <View style={{ flexDirection: 'row' }}>
-
-                            <Entypo 
-                                name="chat" 
-                                size={20} 
-                                color="rgba(255,255,255,.7)" 
-                            />
-
-                            <TextData>Fale Conosco</TextData>
-
-                        </View>
-                        
-                        <AntDesign 
-                            name="right" 
-                            size={20} 
-                            color="rgba(255, 255, 255, .3)" 
-                        />
-
-                    </DataOption>
-
-                    <DataOption
-                        onPress={() => nav.navigate("Termos de Uso")}
-                        activeOpacity={.3}
-                    >
-
-                        <View style={{ flexDirection: 'row' }}>
-
-                            <Ionicons 
-                                name="document-text" 
-                                size={20} 
-                                color="rgba(255,255,255,.7)" 
-                            />
-
-                            <TextData>Termos de Uso</TextData>
-
-                        </View>
-                        
-                        <AntDesign 
-                            name="right" 
-                            size={20} 
-                            color="rgba(255, 255, 255, .3)" 
-                        />
-
-                    </DataOption>
-
-                    <DataOption
-                        onPress={() => nav.navigate("Política de Privacidade")}
-                        activeOpacity={.3}
-                    >
-
-                        <View style={{ flexDirection: 'row' }}>
-
-                            <MaterialIcons 
-                                name="privacy-tip" 
-                                size={24} 
-                                color="rgba(255,255,255,.7)" 
-                            />
-
-                            <TextData>Política de Privacidade</TextData>
-
-                        </View>
-                        
-                        <AntDesign 
-                            name="right" 
-                            size={20} 
-                            color="rgba(255, 255, 255, .3)" 
-                        />
-
-                    </DataOption>
-
-                    <DataOption
-                        onPress={() => signOut()}
-                        activeOpacity={.3}
-                    >
-
-                        <View style={{ flexDirection: 'row' }}>
-
-                            <Entypo 
-                                name="log-out" 
-                                size={24} 
-                                color="rgba(255,255,255,.7)"  
-                            />
-
-                            <TextData>Sair</TextData>
-
-                        </View>
-                        
-                        <AntDesign 
-                            name="right" 
-                            size={20} 
-                            color="rgba(255, 255, 255, .3)" 
-                        />
-
-                    </DataOption>
-
-                </View>
-
+              <TextData>Fale Conosco</TextData>
             </View>
 
-        </ScrollView>
-    )
+            <AntDesign name="right" size={20} color="rgba(255, 255, 255, .3)" />
+          </DataOption>
+
+          <DataOption
+            onPress={() => nav.navigate("Termos de Uso")}
+            activeOpacity={0.3}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons
+                name="document-text"
+                size={20}
+                color="rgba(255,255,255,.7)"
+              />
+
+              <TextData>Termos de Uso</TextData>
+            </View>
+
+            <AntDesign name="right" size={20} color="rgba(255, 255, 255, .3)" />
+          </DataOption>
+
+          <DataOption
+            onPress={() => nav.navigate("Política de Privacidade")}
+            activeOpacity={0.3}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <MaterialIcons
+                name="privacy-tip"
+                size={24}
+                color="rgba(255,255,255,.7)"
+              />
+
+              <TextData>Política de Privacidade</TextData>
+            </View>
+
+            <AntDesign name="right" size={20} color="rgba(255, 255, 255, .3)" />
+          </DataOption>
+
+          <DataOption onPress={() => signOut()} activeOpacity={0.3}>
+            <View style={{ flexDirection: "row" }}>
+              <Entypo name="log-out" size={24} color="rgba(255,255,255,.7)" />
+
+              <TextData>Sair</TextData>
+            </View>
+
+            <AntDesign name="right" size={20} color="rgba(255, 255, 255, .3)" />
+          </DataOption>
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
