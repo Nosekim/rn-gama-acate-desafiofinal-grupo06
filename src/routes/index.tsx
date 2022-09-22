9d3b95d170c62ad3f1b72e0a5cbf66fb30df61f7
Robério <roberiomatos@yahoo.com.br>
2022-09-22 16:23:18 -0300

    added support screen, sign out and corrected bugs in sign in

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,10 @@
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Auth } from "aws-amplify";
+import { useSelector } from "react-redux";
 
 import { IAppState } from "../types";
 
 import AppStack from "./app.stack";
 import LoginStack from "./login.stack";
-
-import {
-  changeIsLoggedIn,
-  changeLoginMethod,
-} from "../store/modules/auth/reducer";
-import {
-  changeIdUser,
-  changeName,
-  changeUserEmail,
-  setToken,
-} from "../store/modules/userProfile/reducer";
 
 const theme = {
   ...DefaultTheme,
@@ -28,34 +15,8 @@ const theme = {
 };
 
 function RootRoutes() {
-  const dispatch = useDispatch();
-  const { isLoggedIn } = useSelector((state: IAppState) => {
-    return state.auth;
-  });
 
-  useEffect(() => {
-    Auth.currentAuthenticatedUser({
-      bypassCache: false,
-    })
-      .then(async (user) => {
-        if (user) {
-          const { signInUserSession } = user;
-          const { idToken } = signInUserSession;
-
-          dispatch(changeIdUser(user.username));
-          dispatch(changeName(user.attributes.name));
-          dispatch(changeUserEmail(user.attributes.email));
-
-          dispatch(changeLoginMethod("email"));
-          dispatch(setToken(idToken.jwtToken));
-
-          setTimeout(() => {
-            dispatch(changeIsLoggedIn(true));
-          }, 50);
-        }
-      })
-      .catch((err) => console.log(err));
-  }, []);
+  const { isLoggedIn } = useSelector((state: IAppState) => state.auth);
 
   return (
     <NavigationContainer theme={theme}>
